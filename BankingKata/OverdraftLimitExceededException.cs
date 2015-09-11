c10983a914b4fc47e6910f6b5179ddd8549ec5f8
James Billings <james.billings@red-gate.com>
2015-09-11 09:49:23 +0100

    Add overdraft exception

---
+++ BankingKata/OverdraftLimitExceededException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingKata
+{
+    public class OverdraftLimitExceededException : Exception
+    {
+        public OverdraftLimitExceededException(string message) : base(message)
+        {
+        }
+    }
+}
