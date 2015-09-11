c98b3240601782e99f73fb4774fe42f05abc8909
James Billings <james.billings@red-gate.com>
2015-09-11 10:07:01 +0100

    Add bankchargedebitentry

---
+++ BankingKata/BankChargeDebitEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankingKata
+{
+    public class BankChargeDebitEntry : DebitEntry
+    {
+        public BankChargeDebitEntry(DateTime transactionDate, Money transactionAmount)
+            : base(transactionDate, transactionAmount)
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("CHRG {0}", base.ToString());
+        }
+    }
+}
